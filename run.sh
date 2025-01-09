490b945db7c0b0068a5ff5dbfeb705e07c48270a
teo693 <marcin.zne@gmail.com>
2025-01-09 09:01:48 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 # run.sh
 #!/usr/bin/with-contenv bashio
+
+bashio::log.info "Starting PSE Energy Status addon..."
+
+# Make sure Python and pip are installed
+apk add --no-cache python3 py3-pip
+
+# Install required packages
+pip3 install aiohttp
+
+bashio::log.info "Running main script..."
 python3 /usr/src/main.py
